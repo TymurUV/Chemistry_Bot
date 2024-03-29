6d5b1f68aa03063c19b6b7c48831b12c7240dc9c
TymurUV <timuron3000@gmail.com>
2024-03-29 11:32:49 -0400

    Chemistry Bot Telegram

---
+++ Chemistry-bot/src/test/java/com/example/Chemistry/bot/ChemistryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chemistry.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
